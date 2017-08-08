c07b23cf51299af813ac3e6b0cf2d3efd0e0e91a
Caiyu Lan <caiyu.lan@citi.com>
2017-08-08 19:52:37 +0100

    Change to use a timer in the receiver

--- Systematica/MessagingSystem/Receiver.cs
+++ Systematica/MessagingSystem/Receiver.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Timer = System.Timers.Timer;
 
 namespace MessagingSystem
 {
@@ -11,23 +12,33 @@ namespace MessagingSystem
     {
         public static event Action<Message> Received;
 
-        private const int LIMIT = 500;
-        private Message[] _messages; 
-        private int _head;
         private readonly object _locker = new object();
+        private Timer _timer;
+        private int _msgCount;
 
         public static void Main(string[] args)
         {
-            Receiver receiver = new Receiver() {_head = 0, _messages = new Message[LIMIT]};
+            Receiver receiver = new Receiver();
             Received += receiver.SaveMsg;
+            
             Sender sender = new Sender();
             new Thread(sender.Run).Start();
-            while (true)
+        }
+
+        public Receiver()
+        {
+            _timer = new Timer() {Enabled = true, Interval = 1000};
+            _timer.Elapsed += OnElapsed;
+            _timer.Start();
+        }
+
+        private void OnElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            lock (_locker)
             {
-                Console.WriteLine("Data received at {0}/s", receiver.GetReceiveRate());
-                //Thread.Sleep(1000);
+                Console.WriteLine("Data received at {0}/s", _msgCount);
+                _msgCount = 0;
             }
-            
         }
 
         public static void OnReceived(Message msg)
@@ -40,35 +51,9 @@ namespace MessagingSystem
             msg.ReceivedTimeStamp = DateTime.Now;
             lock (_locker)
             {
-                // messages are saved cyclically with a buffer size of 500
-                _messages[_head] = msg;
-                _head = (_head + 1) % LIMIT;
+                _msgCount++;
             }
-            //if(GetReceiveRate() > 500)
-            //    Console.WriteLine("<<< Warning: the receiver is accepting data at a rate over the limit! >>>");
-        }
-
-        public int GetReceiveRate()
-        {
-            Message olderMsg, currentMsg;
-            int curr_i, old_i;
-            lock (_locker)
-            {
-                curr_i = (_head - 1 + LIMIT) % LIMIT;
-                old_i = _messages[_head] != null ? _head : 0;
-                currentMsg = _messages[curr_i];
-                olderMsg = _messages[old_i];
-            }
-            //if (olderMsg == null || currentMsg == null)
-            if (olderMsg == null)
-                    return 0;
-
-            double interval = (DateTime.Now - olderMsg.ReceivedTimeStamp).TotalSeconds;
-            int count = curr_i - old_i + 1;
-            count = count > 0 ? count : count + LIMIT;
-
-            return (int) (interval == 0.0 ? 0 : count / interval);
-
+           
         }
 
         
